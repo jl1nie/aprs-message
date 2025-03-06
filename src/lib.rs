b9dfcc76556d4af4ac2b23215489d66f2df37f36
Minoru Tomobe <minoru.tomobe@gmail.com>
2025-03-06 23:12:36 +0900

    Add derive.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use tokio::sync::{mpsc, Mutex};
 use tokio::task::JoinHandle;
 use tracing::{span, Level};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq)]
 pub struct AprsCallsign {
     pub callsign: String,
     pub ssid: Option<u32>,
